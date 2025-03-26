40f8e455d43889b5ba566cbdcae7526d2fead362
Gagan S <gagan.s0191@gmail.com>
2025-03-26 12:27:59 +0530

    Portfolio stage1

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,15 +1,24 @@
-import React, {useState} from "react";
-import Navbar from "./component/Navbar";
+import React from "react";
+import Navbar from "./component/Navbar/Navbar";
+import Hero from "./component/Hero/Hero";
+import About from "./component/About/About"
+import Services from "./component/Services/Services"
+import MyWork from "./component/MyWork/MyWork"
+import Contact from "./component/Contact/Contact"
+import Footer from "./component/Footer/Footer"
 
-const App = ()=>{
-  const [count, setCount] = useState(0);
-
-    return(
-      <div>
-   
-       <Navbar/>
-      </div>
-    )
-  
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About/>
+      <Services/>
+      <MyWork/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
 };
+
 export default App;
